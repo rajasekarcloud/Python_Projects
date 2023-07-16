8873264f0282b16d76399e1f1691289f1174e1e7
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2023-07-15 18:30:20 -0700

    Create strings_length_of_strings.py

---
+++ strings_length_of_strings.py
@@ -0,0 +1,8 @@
+data: str = str(input("Enter a string: "));
+# Step 1
+print(f'Length of string: {len(data)}');
+# Step 2
+count = 0;
+for i in data:
+    count +=1;
+print(f'Length of string: {count}');
