3e75bc1d8dfd29d9563803df31ff0cf403d2d470
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2023-07-17 19:14:50 -0700

    Create thread_demo_simple.py

---
+++ thread_demo_simple.py
@@ -0,0 +1,15 @@
+# Thread pool executor - Demo Without using the thread.
+from concurrent.futures import ThreadPoolExecutor;
+import time;
+values = [3,4,5,6];
+t1 = time.perf_counter();
+def cube(x):
+    for x in values:
+        print(f'Cube of {x}:{x*x*x}');
+    t2 = time.perf_counter();
+    return t2;
+if __name__ == '__main__':
+    t2 = cube(values);
+    print(f'Single Threaded Code Took :{t2 - t1} seconds');
+
+
