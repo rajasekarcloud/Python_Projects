be35dc276703f98ff87b2656220fc10dbfe22a17
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2023-07-22 20:35:22 -0700

    Create dict_check_key_exist.py

---
+++ dict_check_key_exist.py
@@ -0,0 +1,11 @@
+# Challenge: Check if a Key Exists in a Dictionary
+# If the key exists in the dictionary, print True. Else, print False.
+# Input: {"a":4, "b":6}
+# Key is "a"
+# Return True
+data = {"a":4, "b":6};
+for i in data.keys():
+    if i == "a":
+        print(True);
+    else:
+        print(False);
