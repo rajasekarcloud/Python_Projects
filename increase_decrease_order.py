5167011c04f87c1e27092462d36f3ef0f4d8bfc5
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2023-07-23 19:23:15 -0700

    Create increase_decrease_order.py

---
+++ increase_decrease_order.py
@@ -0,0 +1,13 @@
+# Challenge: Increasing or Decreasing Order
+# Write a Python program that determines if three numbers (a, b, and c) are in increasing order, decreasing order, or none.
+# If a > b > c, print "Decreasing Order".
+# If a < b < c, print "Increasing Order".
+# Else, print "None".
+# 1,2,3 - Increasing Order
+a=1;
+b=2;
+c=3;
+if a > b > c:
+    print("Decreasing Order");
+else:
+    print("Increasing Order");
