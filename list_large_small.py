2d06d2b760b754a2ee35eb3caa25373933f49dcf
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2023-07-27 19:56:16 -0700

    Create list_large_small.py

---
+++ list_large_small.py
@@ -0,0 +1,11 @@
+# Challenge: Find the Second Largest Value in a List
+# Option 1
+list=[30,2,10,20,5];
+list = sorted(list);
+print(list);
+print(list[-2]);
+
+# Challenge: Find the Second smallest Value in a List
+list=[30,2,10,20,5];
+list = sorted(list,reverse=True);
+print(list[2]);
