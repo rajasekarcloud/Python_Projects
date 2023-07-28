293d87b4fa1ef18c54a382c11facf57f2eacba3e
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2023-07-27 17:38:33 -0700

    Update recursion_sum_of_number.py

--- recursion_sum_of_number.py
+++ recursion_sum_of_number.py
@@ -15,3 +15,19 @@ def sum(num):
 
 data = [1,2,3];
 print(sum(data));
+
+# Using recursion
+
+# Challenge: Find the Sum of a List using Recursion
+# Write a Python program that finds the sum of a list (or tuple) of numbers recursively.
+# If the list (or tuple) is empty, print 0.
+# [1,2,3] = 6
+
+def sum(data):
+    if len(data) == 0:
+        return 0;
+    else:
+        return data[0] + sum(data[1:]);
+
+data = [1,2,3];
+print(sum(data));
