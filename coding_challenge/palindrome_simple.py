397f82ddc1dd56f90166188d5edfb016d93edef3
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2026-01-05 20:32:51 -0800

    Implement palindrome checking function
    Add function to check if a string is a palindrome.

--- coding_challenge/palindrome_simple.py
+++ coding_challenge/palindrome_simple.py
@@ -1,1 +1,11 @@
-
+def is_palindrome(s):
+    if s == s[::-1]:
+        return "Palindrome"
+    else:
+        return "Not a Palindrome"
+# : → take the whole string
+#
+# -1 → step backwards
+#
+# [::-1] → read the string from end to start
+print(is_palindrome('kayak'))
