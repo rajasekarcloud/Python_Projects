79fa04efaae16bf426f9fb7d06a4f3dfad991523
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2026-01-05 20:27:33 -0800

    Update palindrome.py

--- coding_challenge/palindrome.py
+++ coding_challenge/palindrome.py
@@ -1,1 +1,19 @@
+def is_palindrome(s):
+    left, right = 0, len(s) - 1
+    while left < right:
+        while left < right and not s[left].isalnum(): # Skip non alpha numericals
+            left += 1
 
+        while left < right and not s[right].isalnum(): # Skip non alpha numericals
+            right -= 1
+        print(s[left].lower(),s[right].lower())
+
+        if s[left].lower() != s[right].lower():
+            return False # Not a palindrome
+
+        left += 1
+        right -= 1
+        print(left, right)
+
+    return True
+print(is_palindrome('hello'))
